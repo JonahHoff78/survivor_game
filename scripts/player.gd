1e87832ae3d61dabe0c79f84ef1594071e2d6097
Jonah Hoff <jojoyo98@icloud.com>
2026-02-16 00:12:52 +0000

    Player Movement a2

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,3 +7,18 @@ var player_state
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	
+	if direction.x == 0 and direction.y == 0:
+		player_state = "idle"
+	elif direction.x != 0 or direction.y != 0:
+		player_state = "walking"
+
+	velocity = direction * speed
+	move_and_slide()
+	
+	play_anim(direction)
+	
+func play_anim(dir):
+	if player_state == "idle":
+		$AnimatedSprite2D.play("idle")
+	if player_state == "walking":
+		$AnimatedSprite2D.stop()
