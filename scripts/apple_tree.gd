9e91050154960cb5fcf9d67cef8422d360b6b35f
Jonah Hoff <jojoyo98@icloud.com>
2026-02-22 21:21:36 +0000

    Camera/Items a3 apple tree scene/script

---
+++ scripts/apple_tree.gd
@@ -0,0 +1,7 @@
+extends Node2D
+var state = "no_apples" 
+var player_in_area = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
