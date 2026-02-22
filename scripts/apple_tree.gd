0326107fd5d9e247a919f4dfa3abaceb4eb4e61f
Jonah Hoff <jojoyo98@icloud.com>
2026-02-22 21:28:48 +0000

    Camera/Items a4 apple tree script

--- scripts/apple_tree.gd
+++ scripts/apple_tree.gd
@@ -4,4 +4,30 @@ var player_in_area = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if state == "no_apples":
+		$growth_timer.start()
+		
+func _process(delta):
+	if state == "no_apples":
+		$AnimatedSprite2D.play("no_apples")
+	if state == "apples":
+		$AnimatedSprite2D.play("apples")
+		if player_in_area:
+			if Input.is_action_just_pressed("e"):
+				state = "no_apples"
+				$growth_timer.start()
+
+
+func _on_pickable_area_body_entered(body):
+	if body.has_method("player"):
+		player_in_area = true
+
+
+func _on_pickable_area_body_exited(body):
+	if body.has_method("player"):
+		player_in_area = false
+
+
+func _on_growth_timer_timeout():
+	if state == "no_apples":
+		state = "apples"
