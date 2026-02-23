6b03bcef47241052c1115c61273a7eab0d31a8b3
Jonah Hoff <jojoyo98@icloud.com>
2026-02-23 02:07:15 +0000

    Inventory a14 stick resource script

--- scripts/stick_collectable.gd
+++ scripts/stick_collectable.gd
@@ -4,4 +4,11 @@ extends StaticBody2D
 var player = null
 
 func _on_interactable_area_body_entered(body):
-	pass # Replace with function body.
+	if body.has_method("player"):
+		player = body
+		playercollect()
+		await get_tree().create_timer(0.1).timeout
+		self.queue_free()
+		
+func playercollect():
+	player.collect(item)
